c4172deb1ad4e308850039991134da881f7b4c33
algakz06 <77662392+algakz06@users.noreply.github.com>
2023-12-18 20:42:16 +0300

    db_models was added

--- api/backend/app/models/db_models.py
+++ api/backend/app/models/db_models.py
@@ -3,12 +3,43 @@ from sqlalchemy import (
     Integer,
     String,
     ForeignKey,
-    DateTime,
-    Enum,
-    UniqueConstraint,
+    Numeric,
+    Date
 )
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-import enum
 
 Base = declarative_base()
+
+
+class Active(Base):
+    __tablename__ = "active"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+class SensorType(Base):
+    __tablename__ = "sensor_type"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+class Sensor(Base):
+    __tablename__ = "sensor"
+
+    id = Column(Integer, primary_key=True)
+    active_id = Column(Integer, ForeignKey("active.id"))
+    type = Column(Integer, ForeignKey("sensor_type.id"))
+    name = Column(String)
+
+
+class ActiveIndex(Base):
+    __tablename__ = "active_index"
+
+    id = Column(Integer, primary_key=True)
+    active_id = Column(Integer, ForeignKey("active.id"))
+    sensors_id = Column(Integer, ForeignKey("sensor.id"))
+    value = Column(Numeric)
+    measurement_date = Column(Date)
